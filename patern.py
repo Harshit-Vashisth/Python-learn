19f8187978292f753bba4d16f1599ab1b59abce2
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-03 13:01:21 +0530

    new commit

--- patern.py
+++ patern.py
@@ -7,3 +7,32 @@ while i<n :
         j=j+1
     print(" ")
     i=i+1
+
+n=int(input())
+i=0
+while i<n :
+    j=0
+    while j<n:
+        print(j+1,end=" ")
+        j=j+1
+    print(" ")
+    i=i+1
+
+n=int(input())
+i=0
+while i<n :
+    j=0
+    while j<n:
+        print(i+1,end=" ")
+        j=j+1
+    print("")
+    i=i+1
+n=int(input())
+i=0
+while i<n :
+    j=0
+    while j<n:
+        print(n-j,end=" ")
+        j=j+1
+    print("")
+    i=i+1
