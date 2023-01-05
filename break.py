27470c7e311019079c117cb0734bb768cea28ea5
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-05 22:48:55 +0530

    new commit

---
+++ break.py
@@ -0,0 +1,13 @@
+for i in range( 1,10,1):
+    if(i==6):
+        break
+    print(i)
+    i=i+1
+
+    #square integral
+ans=0
+n=int(input())
+for i in range(1,n,1):
+    if i*i<=n:
+        ans=i;
+print(ans)
--- print nth.py
+++ print nth.py
@@ -7,3 +7,25 @@ for i in range(1,n*4,1):
         c=c+1
         if(c==n):
             break
+
+#check if numbe is prime
+n=int(input())
+flag=False
+for i in range(2,n,1):
+    if(n%i==0):
+        flag=True
+if(flag!=True):
+    print("Prime")
+else:
+    print("No ")
+
+#patttern using for
+n=int(input())
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for s in range( i,2*i,1):
+        print(s,end=" ")
+    for z in range(2*i-2,i-1,-1):
+        print(z,end="")
+    print()
