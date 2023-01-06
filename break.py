89d9d179ae4659ca18470a8aee5884d6903950d8
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-06 14:04:23 +0530

    new commit

--- break.py
+++ break.py
@@ -11,3 +11,10 @@ for i in range(1,n,1):
     if i*i<=n:
         ans=i;
 print(ans)
+
+##continue use to skip particuler itteration
+
+for i in range(1,10):
+    if i==5:
+        continue
+    print(i)
