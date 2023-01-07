fe177bb754bc1261188106813d81f7f18a40d8c1
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-07 16:10:52 +0530

    new commit

--- questiononreplace.py
+++ questiononreplace.py
@@ -5,3 +5,10 @@ i=s.find('x')
 if(i!=-1):
     s=s[:i]+'y'+s[i+1:]
 print(s)
+
+s=input()
+
+for i in range(0,len(s)):#len is used for lenght
+    if(s[i]=='x'):
+        s=s[:i]+'y'+s[i+1:]
+print(s)
