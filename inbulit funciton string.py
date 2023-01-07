1ed3ae914cff30b181c87b8f3b5c647b94535661
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-07 15:46:55 +0530

    new commit

---
+++ inbulit funciton string.py
@@ -0,0 +1,26 @@
+a="hello"
+b="harshit"
+print(a+b)
+#or
+# a=a+b
+print(a)
+
+#it get concatinate note new string named helloharshit is created not the orginal string is change
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a!=b)
+print(a==b)
+
+
+###############################
+
+##LOWER CASE AND UPPER CASE
+
+a="this is a string "
+a=a.upper()
+print (a)
+a=a.lower()
+print(a)
