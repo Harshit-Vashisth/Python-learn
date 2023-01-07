75de355a9c0534c72ed6379fa7897c4a5369048a
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-07 15:41:08 +0530

    new commit

---
+++ string.py
@@ -0,0 +1,19 @@
+s="harshit"
+s='harshit'
+#both of them are valid
+print(s[3])
+#  s[3]='k' this is not valid as u cannot change the string but u can modify the variable
+
+s='roghit'
+print(s)
+
+s[3]
+# this is a stirng as there is nothing called char in python everything is string
+
+
+#s='Harhi's'
+#in this there is problem that compiler is not able to indentify its as a part of stirng
+# one way is to make " ' " like this  other way is to u \ whihc is an ecsape seq
+
+s=' harshit is a good bo\'s  '
+print(s)
