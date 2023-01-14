d46dbb3e2416629744521db8360e38e8fd9d5e7b
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-14 12:54:36 +0530

    new commit

--- sum rowwise.py
+++ sum rowwise.py
@@ -1,16 +1,15 @@
-def row_sum(a):
+
+def row_print(a):
     m=len(a)
     if(m==0):
         return
-    n=len(a[0])
-    for i in range(m):
+    for i in range (m):
         sum=0
-        for j in range(n):
+        for j in range(5):
             sum+=a[i][j]
         print(sum)
 
 
-a=[(j+1 for j in range(5)) for i in range(5)]
+a=[[j+1 for j in range(5)] for i in range(5)]
 print(a)
-print(row_sum(a))
-
+print(row_print(a))
