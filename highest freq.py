1550405ca1d7811ef4775e5fb6baefc1c3a541f0
Harshit_Vashisth <harshitvashisth76@gmail.com>
2023-01-23 01:17:13 +0530

    BY harshit

--- highest freq.py
+++ highest freq.py
@@ -7,7 +7,8 @@ def printwords(s):
     for w in d:
         if(d[w]>max):
             ans=w
+            max=d[w]
     return ans
 
-s=[1,2,2,3,2,2,3,4,5,6,8,4,0,1,1,2]
+s=[1,2,2,3,2,2,3,4,5,6,8,4,0,1,1,2,4,4,4,4,4,4,4,4,4,4]
 print(printwords(s))
